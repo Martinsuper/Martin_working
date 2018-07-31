5e2254caae67dbbb5f60a857dd55f27f97d6955d
Martin <2014551318@smail.xtu.edu.cn>
2018-07-31 10:26:33 +0800

    基于spring mvc的helloworld

---
+++ HelloWorld_1/src/main/java/myHelloWorld/HelloWorldController.java
@@ -0,0 +1,22 @@
+package myHelloWorld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName HelloWorldController
+ * @Description
+ * @Author luyao.duan
+ * @Date 2018/7/31 9:47
+ **/
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = "/doICando")
+    public String sayHello(){
+
+        System.out.println("你好！这个是一个测试");
+        return "SpringHello";
+    }
+
+}
---
+++ HelloWorld_1/src/main/webapp/WEB-INF/views/SpringHello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Martin_working
+  Date: 2018/7/31
+  Time: 9:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>my hello world</title>
+</head>
+<body>
+    <h1>I think spring mvc is very interesting!</h1>
+</body>
+</html>
